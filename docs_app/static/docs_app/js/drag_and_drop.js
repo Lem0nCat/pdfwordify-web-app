30157cd99de1d6d68058a83cfb4c99b3a0f341d7
LemonCat <ilnaz.imamov77@gmail.com>
2024-05-05 01:58:27 +0300

    Add: header icon, icon change when hovering over the file drop area

--- docs_app/static/docs_app/js/drag_and_drop.js
+++ docs_app/static/docs_app/js/drag_and_drop.js
@@ -7,11 +7,19 @@ dropArea.addEventListener('dragover', (event) => {
     event.preventDefault();
     dropArea.classList.add('active');
     dropArea.querySelector('p').textContent = "Release to upload the file";
+
+    // Change Icon
+    var icon = document.getElementById('dynamicIcon');
+    icon.classList.replace('bi-file-earmark-pdf-fill', 'bi-file-earmark-arrow-down-fill');
 });
 
 dropArea.addEventListener('dragleave', () => {
     dropArea.classList.remove('active');
     dropArea.querySelector('p').textContent = "Drag the PDF file here or click to select a file";
+    
+    // Change Icon
+    var icon = document.getElementById('dynamicIcon');
+    icon.classList.replace('bi-file-earmark-arrow-down-fill', 'bi-file-earmark-pdf-fill');
 });
 
 dropArea.addEventListener('drop', (event) => {
